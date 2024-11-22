f73787ed797d523f10ee9596f458a2a502f9e15b
JulijaBeikmane <131656982+JulijaBeikmane@users.noreply.github.com>
2024-11-22 09:28:20 +0200

    Create 1_dati.py

---
+++ 1_dati.py
@@ -0,0 +1,14 @@
+dotie_jautajumi = [
+    {"jaut": "Cilvēkam ir 2 kājas", "atbilde": "True"},
+    {"jaut": "Šodien ir pirmdiena", "atbilde": "False"},
+    {"jaut": "Muhammeds Ali ir boksieris", "atbilde": "True"},
+    # {"jaut": "A cheetah can run faster than a lion.", "atbilde": "True"},
+    # {"jaut":"Monkeys swing through the trees with their tails", "atbilde": "False"},
+    # {"jaut": "Lobsters are red when they’re alive", "atbilde": "False"},
+    # {"jaut": "The Eiffel Tower is in France", "atbilde": "True"},
+    # {"jaut": "A triangle has four sides", "atbilde": "False"},
+    # {"jaut": "Electricity always flows from positive to negative charges", "atbilde": "False"},
+    # {"jaut": "White light is made up of all the colors of the rainbow", "atbilde": "True"},
+    # {"jaut": "Water boils at 100 degrees Celsius or 212 degrees Fahrenheit", "atbilde": "True"},
+    # {"jaut": "All metals are magnetic", "atbilde": "False"}
+]
