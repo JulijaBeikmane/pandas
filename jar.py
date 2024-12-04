c86b81e27e097dff6d23c10bf80b9a89c6977334
JulijaBeikmane <131656982+JulijaBeikmane@users.noreply.github.com>
2024-12-04 10:20:02 +0200

    Create jar.py

---
+++ jar.py
@@ -0,0 +1,22 @@
+class Jar:
+    def __init__(self, capacity=12):
+        if capacity < 0:
+            raise ValueError
+        self.capacity = capacity
+
+    def __str__(self):
+        ...
+
+    def deposit(self, n):
+        ...
+
+    def withdraw(self, n):
+        ...
+
+    @property
+    def capacity(self):
+        ...
+
+    @property
+    def size(self):
+        ...
