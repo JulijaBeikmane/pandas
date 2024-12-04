d16e470b94ea0bf281fac9463476e71a622a2dcf
JulijaBeikmane <131656982+JulijaBeikmane@users.noreply.github.com>
2024-12-04 10:07:23 +0200

    Update season.py

--- season.py
+++ season.py
@@ -1,15 +1,19 @@
 from datetime import date
 import sys
+import inflect
 
 def main():
     dzims_datums = get_user_input()
     vecums_minutes = calculate_age_in_minutes(dzims_datums)
-    print(vecums_minutes)
+    vardos = number_to_words(vecums_minutes)
+    print(vardos + " minutes")
 def get_user_input():
     try:
-        ievade = input("Ievadi dzimšanas datus formā GGGG-MM-DD ")
+        ievade = input("Ievadi dzimšanas datus formā GGGG-MM-DD: ")
         gads, menesis, diena = map(int, ievade.split("-"))
         dzims_datums = date(gads, menesis, diena)
+        # gads, menesis, diena = ievade.split("-")
+        # dzims_datums = date(int(gads), int(menesis), int(diena))
         return dzims_datums
     except ValueError:
         print("Invalid date")
@@ -21,8 +25,11 @@ def calculate_age_in_minutes(dzims_datums):
     vecums_minutes = vecums.days * 24 * 60
     return vecums_minutes
 
-def number_to_words():
-    pass
+def number_to_words(skaitlis):
+    parveide = inflect.engine()
+    vardi = parveide.number_to_words(skaitlis).replace(" and ", " ")
+    return vardi.capitalize()
+
 
 if __name__ == "__main__":
     main()
