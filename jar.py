6e3ce92dfe6900221612d08d67dd548f6943d676
JulijaBeikmane <131656982+JulijaBeikmane@users.noreply.github.com>
2024-12-06 09:01:35 +0200

    Update jar.py

--- jar.py
+++ jar.py
@@ -1,22 +1,37 @@
 class Jar:
     def __init__(self, capacity=12):
-        if capacity < 0:
-            raise ValueError
-        self.capacity = capacity
+        if capacity >= 0:
+            self._capacity = capacity
+            self._size = 0
+        else:
+            raise ValueError("Capacity must be a non-negative integer.")
 
     def __str__(self):
-        ...
+        return "🍪" * self.size
 
     def deposit(self, n):
-        ...
+        if n > self.capacity or (self.size + n) > self.capacity:
+            raise ValueError("Too much cookies")
+        else:
+            self._size += n
 
     def withdraw(self, n):
-        ...
-
+        if n > self.size:
+            raise ValueError("Number of cookies to withdraw must be a non-negative integer.")
+        else:
+            self._size -= n
+        # if self.capacity < self.capacity:
+        #     raise ValueError("Number of cookies to withdraw must be a non-negative integer.")
+        
     @property
     def capacity(self):
-        ...
+        return self._capacity
 
     @property
     def size(self):
-        ...
+        return self._size
+    
+jar = Jar(18)
+jar.deposit(10)
+jar.withdraw(3)
+print(jar)
